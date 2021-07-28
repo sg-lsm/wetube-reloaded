b12af4256eda2fb63ffcf6d1ba04f355376e1e0b
sg-lsm <70528905+sg-lsm@users.noreply.github.com>
2021-07-29 03:01:55 +0900

    upload fix

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,18 @@
 import multer from 'multer';
+import multerS3 from 'multer-s3';
+import aws from 'aws-sdk';
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: process.env.AWS_ID,
+    secretAccessKey: process.env.AWS_SECRET,
+  },
+});
+
+const multerUploader = multerS3({
+  s3: s3,
+  bucket: 'wetube-reloaded-lsm',
+});
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
@@ -20,7 +34,7 @@ export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
   } else {
-    req.flash("error", "not Authorized")
+    req.flash('error', 'not Authorized');
     return res.redirect('/');
   }
 };
@@ -30,10 +44,12 @@ export const avatarUpload = multer({
   limits: {
     fileSize: 3000000,
   },
+  storage: multerUploader,
 });
 export const videoUpload = multer({
   dest: 'uploads/videos/',
   limits: {
     fileSize: 10000000,
   },
+  storage: multerUploader,
 });
